b7cbef99cf67db4f2b56afdaef084fb93cd80475
hslee15 <lhs01019@kbu.ac.kr>
2025-08-22 15:16:08 +0900

    n

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import Button from '../components/Button'
 import DiaryList from '../components/DiaryList'
 import { DiaryStateContext, ModeContext } from '../App'
 import useTitle from '../hook/useTitle'
-import thumbnail from '../assets/thumbnail.png'
 
 const Home = () => {
     const data=useContext(DiaryStateContext)
@@ -53,11 +52,6 @@ const Home = () => {
                 {mode === "light" ? "🌙 다크 모드" : "☀️ 라이트 모드"}
             </button>
 
-            <img
-                src={thumbnail}
-                alt="메인 썸네일"
-                style={{ width: "200px", margin: "20px auto", display: "block" }}
-            />
             <Header
             leftChild={<Button text={"<"} onClick={onDecreamentMonth}/>}
             title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth()+1}월`}
